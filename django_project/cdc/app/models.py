4d4fdf98e953be4cef254ee541070fa7d88e267a
Eduardo Martins Pinto <eduardo.90.martins@gmail.com>
2021-03-29 08:50:01 +0200

    2th challenge: unique author

--- django_project/cdc/app/models.py
+++ django_project/cdc/app/models.py
@@ -5,5 +5,5 @@ from django.db.models import DateTimeField, CharField, EmailField
 class Author(models.Model):
     created_at: DateTimeField = models.DateTimeField(name='created_at', auto_now=True)
     description: CharField = models.CharField(max_length=400)
-    email: EmailField = models.EmailField(name='email')
+    email: EmailField = models.EmailField(name='email', unique=True)
     name: CharField = models.CharField(max_length=100, name='name')
